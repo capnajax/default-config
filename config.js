e36e073f71401c417063429327462d74cbd5a0ea
Thomas Wilkinson <thomas.wilkinson@us.ibm.com>
2020-10-06 18:38:06 -0500

    initial import

---
+++ config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+const _ = require('lodash');
+
+let configFile;
+let envName;
+
+function setupConfigFilename() {
+  let configFilename = null;
+  for(let i = 2; i < process.argv.length; i++) {
+    if (process.arv[i].startsWith('--config=')) {
+      configFilename = process.argv[i].replace(/^--config=/, '');
+      break;
+    }
+  }
+
+  if (!configFilename) {
+    configFilename = 
+        process.env.CONFIG || path.join(__dirname, '..', 'config.yaml');
+  }
+
+  return configFilename;
+}
+
+function setupEnvName() {
+  let envName = null;
+  for(let i = 2; i < process.argv.length; i++) {
+    if (process.arv[i].startsWith('--env=')) {
+      result = process.argv[i].replace(/^--env=/, '');
+      break;
+    }
+  }
+
+  if (!envName) {
+    envName = process.env.NODE_ENV || 'local';
+  }
+
+  return envName;
+}
+
+function setup() {
+  let configFilename = setupConfigFilename();
+  
+  configFile = YAML.parse(fs.readFileSync(configFilename));
+  envName = setupEnvName();
+}
+
+function config(path, defaultValue) {
+  let envPath = `${envName}.${path}`;
+  let result = defaultValue;
+  if(envName && _.has(configFile.environments, envPath)) {
+    result = _.get(configFile.environments, envPath);
+  } else if (_.has(configFile.default, path)) {
+    result = _.get(configFile.default, path);
+  }
+  return result;
+}
+
+setup();
+module.exports = config;
